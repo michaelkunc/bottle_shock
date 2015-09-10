0b6d15626e24a66954ccb832d2ea77efe8086eee
michaelkunc <mjkunc@gmail.com>
2015-09-10 09:47:15 -0500

    improved item scrape

--- item_detail.rb
+++ item_detail.rb
@@ -11,17 +11,15 @@ CSV.foreach('../brands.csv') do |row|
   urls << row[1]
 end
 
-urls[3401..3500].each do |url|
+urls[0..-1].each do |url|
   doc = Nokogiri::HTML(open(url))
   name = doc.css('h1').text
   item_number = doc.css('div.productInfo > h3').text.gsub("Item #", '').to_i
-  size = doc.css('div.productInfo > ul > li' )[0].text.gsub("Size: ", '')
-  type = doc.css('div.productInfo > ul > li' )[1].text.gsub("Type: ",'')
-  # country = doc.css('div.productInfo > ul > li' )[2].text.gsub("Country: ", '')
-  # proof = doc.css('div.productInfo > ul > li' )[4].text.gsub("Proof: ", '').to_i
+  doc.css('div.productInfo > ul > li' )[0] != nil ? size = doc.css('div.productInfo > ul > li' )[0].text.gsub("Size: ", '') : size = nil
+  doc.css('div.productInfo > ul > li' )[1] != nil ? type = doc.css('div.productInfo > ul > li' )[1].text.gsub("Type: ",'') : type = nil
 
   brand_details[name] = [url, item_number, size, type]
-  # sleep 1
+  sleep 1
 end
 
 CSV.open("brand_details.csv", "a") {|csv| brand_details.to_a.each {|elem| csv << elem }}
